bafb23b06b1a4c24ea6bdf64453b674927802a56
Zer0SkiLL <retowidmer95@hotmail.com>
2022-03-30 16:30:05 +0200

    custom 404 page and added FA icons

---
+++ frontend/pages/404.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { FaExclamationTriangle } from 'react-icons/fa';
+import Link from 'next/link';
+
+import styles from '../styles/404.module.css';
+
+import Layout from '../components/Layout';
+
+export default function NotFoundPage() {
+    return (
+        <div>
+            <Layout>
+                <div className={styles.error}>
+                    <h1>
+                        {' '}
+                        <FaExclamationTriangle></FaExclamationTriangle>404
+                    </h1>
+                    <h4>Nicht das geringste zu sehen</h4>
+                    <Link href="/">Go Back Home</Link>
+                </div>
+            </Layout>
+        </div>
+    );
+}
