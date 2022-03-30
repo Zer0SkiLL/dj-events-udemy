2182a5b8d4aa602e0be190d22f4de6bc273d26c6
Zer0SkiLL <retowidmer95@hotmail.com>
2022-03-30 16:48:40 +0200

    added showcase to only display on homepage and added some sample images

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -1,11 +1,16 @@
 import React from 'react';
+import { useRouter } from 'next/router';
+
 import styles from '@/styles/Layout.module.css';
 
 import Head from 'next/head';
 import Header from './Header';
 import Footer from './Footer';
+import Showcase from './Showcase';
 
 export default function Layout({ title, keywords, description, children }) {
+    const router = useRouter();
+
     return (
         <div>
             <Head>
@@ -13,8 +18,13 @@ export default function Layout({ title, keywords, description, children }) {
                 <meta name="description" content={description}></meta>
                 <meta name="keyords" content={keywords}></meta>
             </Head>
+
             <Header></Header>
+
+            {router.pathname === '/' && <Showcase></Showcase>}
+
             <div className={styles.container}>{children}</div>
+
             <Footer></Footer>
         </div>
     );
