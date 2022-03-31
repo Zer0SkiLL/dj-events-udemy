48ce3af4e361305e0a45a2769de596e057143511
Zer0SkiLL <retowidmer95@hotmail.com>
2022-03-31 07:37:52 +0200

    implemented events detail with mock data

--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,11 +1,35 @@
 import Layout from '@/components/Layout';
+import EventItem from '@/components/EventItem';
 
-export default function Home() {
+import { API_URL } from '@/config/index';
+import Link from 'next/link';
+
+export default function Home({ events }) {
+    console.log(events);
     return (
         <div>
             <Layout>
-                <h1>Home</h1>
+                <h1>Upcoming Events</h1>
+                {events.length === 0 && <h3>No upcoming Events</h3>}
+
+                {events.map((evt) => (
+                    <EventItem key={evt.id} evt={evt}></EventItem>
+                ))}
+                {events.length > 0 && (
+                    <Link href="/events">
+                        <a className="btn-secondary">Show All</a>
+                    </Link>
+                )}
             </Layout>
         </div>
     );
+}
+
+export async function getServerSideProps() {
+    const res = await fetch(`${API_URL}/api/events`);
+    const events = await res.json();
+
+    return {
+        props: { events },
+    };
 }
