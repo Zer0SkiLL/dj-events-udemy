f3176c438b9adcc9e197732b8c175e6d50554465
Zer0SkiLL <retowidmer95@hotmail.com>
2022-04-06 19:27:47 +0200

    backend added events create/update/delete wich checks if the event belongs to the user that requests

--- backend/src/api/event/controllers/event.js
+++ backend/src/api/event/controllers/event.js
@@ -7,6 +7,70 @@
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController("api::event.event", ({ strapi }) => ({
+  // Create Event with linked user
+  async create(ctx) {
+    let entity;
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      data.user = ctx.state.user.id;
+      entity = await strapi.services.event.create(data, { files });
+    } else {
+      ctx.request.body.user = ctx.state.user.id;
+      entity = await strapi.services.event.create(ctx.request.body);
+    }
+    return sanitizeEntity(entity, { model: strapi.models.event });
+  },
+
+  // update user event
+  async update(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+    const event = await strapi.db.query("api::event.event").findOne({
+      where: {
+        user: { id: ctx.state.user.id },
+        id: ctx.params.id,
+      },
+    });
+
+    if (!event) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    if (ctx.is("multipart")) {
+      const { data, files } = parseMultipartData(ctx);
+      entity = await strapi.services.event.update({ id }, data, {
+        files,
+      });
+    } else {
+      entity = await strapi.services.event.update({ id }, ctx.request.body);
+    }
+
+    return sanitizeEntity(entity, { model: strapi.models.event });
+  },
+
+  // Delete user event
+  async delete(ctx) {
+    const { id } = ctx.params;
+
+    let entity;
+
+    const [event] = await strapi.services.event.findOne({
+      where: {
+        user: { id: ctx.state.user.id },
+        id: ctx.params.id,
+      },
+    });
+
+    if (!event) {
+      return ctx.unauthorized(`You can't update this entry`);
+    }
+
+    entity = await strapi.services.event.delete({ id });
+
+    return sanitizeEntity(entity, { model: strapi.models.event });
+  },
+
   // Get logged in users
   async me(ctx) {
     const user = ctx.state.user;
